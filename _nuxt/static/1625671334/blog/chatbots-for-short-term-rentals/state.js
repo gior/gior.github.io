f8eecc7a2a78166dc5a3edfecc0b4defcec732de
Giorgio Sidari <giorgio@sidari.it>
2021-07-07 17:31:14 +0200

    78aa980

---
+++ _nuxt/static/1625671334/blog/chatbots-for-short-term-rentals/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625671334",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fchatbots-for-short-term-rentals",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b47e0527"}}}}(null));
---
+++ _nuxt/static/1625671334/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625671334",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b47e0527"}}}}(null,"\u002F"));
