84b90a196236490a92273c5a691d51f6e23eafef
Giorgio Sidari <giorgio@sidari.it>
2026-02-26 16:10:12 +0100

    Deploy static site

---
+++ _nuxt/2osQWzCR.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./D7ob66UM.js";import{o as s,s as n,w as f,y as p}from"./Dwdxqruj.js";const l={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{l as default};
